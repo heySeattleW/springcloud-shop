501f94b902d9e313d609717a5567bd7e849e9fcd
hey <354034910@qq.com>
2018-01-04 14:42:43 +0800

    service

--- service-test/src/main/java/com/hey/controller.java
+++ service-test/src/main/java/com/hey/controller.java
@@ -1,6 +1,7 @@
 package com.hey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by hey on 2018/1/4.
  */
 @RestController
+@RefreshScope
 public class controller {
 
     @Value("${value}")
